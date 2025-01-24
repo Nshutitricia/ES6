74c7478f11d6ff76fb4ea3a0d5fc2b2fc93be4d6
Nshutitricia <nshutricia@gmail.com>
2025-01-24 13:45:42 +0200

    classes

--- Classes.js
+++ Classes.js
@@ -4,4 +4,25 @@ class Book{
         this.author = author;
         this.year = year;
     }
+    getsummary(){
+        return `${this.title} was written by ${this.author} in ${this.year}`  
+    }
+    getAge(){
+        const years = new Date().getFullYear() - this.year;
+        return `${this.title} is ${years} years old`
+    }
+    revise(newYear){
+        this.year=newYear;
+        this.revised = true;
+    }
+    static topBook(){ // You don't have to instantiate it you call it on the conlstructor
+        return "This is my book"
+    }
+
 }
+//Instantiate Object
+const book1 = new Book('Book One', 'Tricia', '2020')
+console.log(book1)
+book1.revise('2023')
+console.log(book1)
+console.log(Book.topBook())
